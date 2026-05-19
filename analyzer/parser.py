7ec897a6615029b9e783b300ddef34a22230cee1
coolpritam <159032508+coolpritam@users.noreply.github.com>
2026-05-19 20:31:47 +0530

    Enhance error handling in read_log_file function
    Add error handling for general exceptions in read_log_file.

--- analyzer/parser.py
+++ analyzer/parser.py
@@ -2,6 +2,11 @@ def read_log_file(file_path):
     try:
         with open(file_path, "r") as file:
             return file.readlines()
+
     except FileNotFoundError:
         print(f"[ERROR] File not found: {file_path}")
+        return []
+
+    except Exception as error:
+        print(f"[ERROR] {error}")
         return []
