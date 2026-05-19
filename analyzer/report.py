f422deb4481a80fc4f902cd1ffdb150572918fe0
coolpritam <159032508+coolpritam@users.noreply.github.com>
2026-05-19 20:32:30 +0530

    Update report.py

--- analyzer/report.py
+++ analyzer/report.py
@@ -1,6 +1,6 @@
 def generate_report(issues):
 
-    print("========== Analysis Report ==========\n")
+    print("\n========== Analysis Report ==========\n")
 
     print(f"MAP Operations Detected : {issues['map_operations']}")
     print(f"TCAP Aborts Detected    : {issues['tcap_aborts']}")
@@ -17,3 +17,5 @@ def generate_report(issues):
 
     if issues["resource_limitations"] > 0:
         print("- Check SMSC/IPSTG resource utilization")
+
+    print("\n========== Analysis Completed ==========\n")
